acaad563193abb8f26f19aac99458dcedcb75bec
Claudio Altamura <claudio@claudioaltamura.de>
2020-04-26 14:19:10 +0200

    chg: added additional methods

--- src/main/java/de/claudioaltamura/java/multithreading/completionstage/CompletableFutureWithEncapsulatedLogic.java
+++ src/main/java/de/claudioaltamura/java/multithreading/completionstage/CompletableFutureWithEncapsulatedLogic.java
@@ -7,4 +7,16 @@ public class CompletableFutureWithEncapsulatedLogic {
   public CompletableFuture<String> calculateAsync() {
     return CompletableFuture.supplyAsync(() -> "hello, world");
   }
+
+  public CompletableFuture<String> calculateAsync(String str) {
+    return CompletableFuture.supplyAsync(() -> str);
+  }
+
+  public CompletableFuture<String> calculateAsyncWithException(String str) {
+    return CompletableFuture.supplyAsync(
+        () -> {
+          if (str == null) throw new IllegalArgumentException();
+          return "hello " + str;
+        });
+  }
 }
